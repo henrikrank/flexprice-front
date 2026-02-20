2467d7d5f297496529fddd1b8c47cb7ed8a364a1
Henrik Rank <rank.henrik@gmail.com>
2026-02-20 15:23:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,23 @@ import { defineConfig } from 'vite';
 const meta = JSON.parse(fs.readFileSync('./public/meta.json', 'utf8'));
 
 export default defineConfig({
-	plugins: [react()],
-	define: {
-		__APP_VERSION__: JSON.stringify(meta.versionId),
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	server: {
-		cors: {
-			origin: 'http://localhost:3000',
-			methods: ['GET', 'POST'],
-		},
-		host: 'localhost',
-	},
+  plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(meta.versionId),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 3000,
+    allowedHosts: ['flexprice.henrikrank.ee'],
+    cors: {
+      // IMPORTANT: in production you usually don't want localhost here
+      origin: ['https://flexprice.henrikrank.ee'],
+      methods: ['GET', 'POST'],
+    },
+  },
 });
